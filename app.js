fe97de39f20e5ad3fa7e7cd825cd53bfdd6ef7a7
Ayush Agrawal <ayushthetechie@gmail.com>
2017-07-06 01:42:23 +0530

    Delete Function Added

--- app.js
+++ app.js
@@ -87,6 +87,17 @@ app.put("/blogs/:id", function(req, res) {
     });
 })
 
+app.delete("/blogs/:id", function(req, res){
+    Blog.findByIdAndRemove(req.params.id, function(err){
+        if (err){
+            console.log(err);
+            res.redirect('/blogs');
+        } else {
+            res.redirect('/blogs');
+        }
+    })
+
+});
 
 app.listen(3000, function(){
   console.log("Server is running.")
